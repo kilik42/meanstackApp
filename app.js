0234eb65922d417ef8f61d82542958f7d59d5c3c
marvin evins <maevins@cps.edu>
2018-04-22 11:17:34 -0500

    day one of creating another mean app
    day one set up express and mongo

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+
+//connect to database
+mongoose.connect(config.database);
+
+//on connection
+mongoose.connection.on('connected', ()=> {
+  console.log('connected to database'+config.database);
+});
+
+//on error
+mongoose.connection.on('error', (err)=> {
+  console.log('database error: '+err);
+});
+
+
+
+const app = express();
+const users  = require('./routes/users');
+
+
+const port = 3000;
+
+app.use(cors());
+
+//body bodyParser middleware
+
+
+//set static folder
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(bodyParser.json());
+
+//index route
+
+app.get('/', (req, res)=> {
+  res.send('invalid endpoint');
+});
+
+
+app.use('/users', users);
+
+//start server
+app.listen(port, ()=> {
+    console.log('server started on port'+ port);
+});
---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const router = express.Router();
+
+//register
+router.get('/register', (req, res, next)=> {
+    res.send('REGISTER');
+});
+
+
+//authenticate
+router.get('/authenticate', (req, res, next)=> {
+    res.send('authenticate');
+});
+
+
+//profile
+router.get('/profile', (req, res, next)=> {
+    res.send('profile');
+});
+
+//validate
+router.get('/validate', (req, res, next)=> {
+    res.send('validate');
+});
+
+
+module.exports = router;
